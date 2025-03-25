8305505409fedb51271fc7f6088b6d3996acfc1a
Lauracpg <laurameihua@gmail.com>
2025-03-25 17:25:17 +0100

    baseline

---
+++ src/baseline_ebaluazioa.java
@@ -0,0 +1,72 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import weka.classifiers.Evaluation;
+import weka.classifiers.functions.LinearRegression;
+import weka.core.Instances;
+import weka.core.SerializationHelper;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class baseline_ebaluazioa {
+    public static void main(String[] args){
+        if(args.length < 4){
+            System.out.println("java -jar baseline_ebaluazioa.jar <train.arff><dev.arff><EvaluationBaseline.txt>");
+            return;
+        }
+        String trainPath = args[0];
+        String devPath = args[1];
+        String modelPath = args[2];
+        String ebaluazioaPath = args[3];
+
+        try{
+            DataSource srcTrain = new DataSource(trainPath);
+            Instances train = srcTrain.getDataSet();
+
+            if(train.classIndex() == -1){
+                train.setClassIndex(train.numAttributes() - 1);
+            }
+
+            DataSource srcDev = new DataSource(devPath);
+            Instances dev = srcDev.getDataSet();
+
+            if(dev.classIndex() == -1){
+                dev.setClassIndex(dev.numAttributes() - 1);
+            }
+
+            LinearRegression model = new LinearRegression();
+            model.buildClassifier(train);
+
+            SerializationHelper.write(modelPath, model);
+            System.out.println("Eredua hemen gorde da: " + modelPath);
+           
+            Evaluation eval = new Evaluation(train);
+            eval.evaluateModel(model, dev);
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			String exekuzioData = sdf.format(new Date());
+
+            try(PrintWriter writer = new PrintWriter(new FileWriter(ebaluazioaPath))){ 
+				writer.println("==== Linear Regression ebaluazioa ====");
+				writer.println("Exekuzio data: " + exekuzioData);
+
+				writer.println("\n--- Nahasmen matrizea ---");
+				writer.println(eval.toMatrixString());
+				
+				writer.println("\n--- Precision klasearen balio bakoitzeko ---");
+		        writer.println(eval.toClassDetailsString());
+
+		        writer.println("\n--- Weighted Average ---");
+		        writer.println("Precision: " + eval.weightedPrecision());
+		        writer.println("Recall: " + eval.weightedRecall());
+		        writer.println("F-Measure: " + eval.weightedFMeasure());
+		        writer.println("\nEbaluazio-emaitzak amaituta.");
+			}
+            System.out.println("Ebaluazio osatua. Emaitzak gorde dira hemen: " + ebaluazioaPath);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/baseline_iragarpenak_egin.java
@@ -0,0 +1,49 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+import weka.classifiers.functions.LinearRegression;
+import weka.core.Instances;
+import weka.core.SerializationHelper;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class baseline_iragarpenak_egin {
+    public static void main(String[] args){
+        if(args.length < 3){
+            System.out.println("java -jar baseline_iragarpenak_egin.jar <testBlind.arff><model><baseline_iragarpenak.txt>");
+            return;
+        }
+        String testPath = args[0];
+        String modelPath = args[1];
+        String iragarpenakPath = args[2];
+
+        try{ 
+            // Linear regression eredua kargatu:
+        	LinearRegression model = (LinearRegression) SerializationHelper.read(modelPath);
+        	
+        	//test blind kargatu:
+        	DataSource src = new DataSource(testPath);
+        	Instances blindData = src.getDataSet();
+        	
+        	if (blindData.classIndex() == -1) {
+        		blindData.setClassIndex(blindData.numAttributes() - 1);
+            }
+
+            try (PrintWriter writer = new PrintWriter(new FileWriter(iragarpenakPath))) {
+            	writer.println("==== Linear Regression Iragarpenak ====");
+                
+                //Instantzia bakoitza zeharkatu eta iragarpena egin:
+                for(int i = 0; i < blindData.numInstances(); i++) {
+                	double predIndex = model.classifyInstance(blindData.instance(i));
+                	String predictedClass = blindData.classAttribute().value((int) predIndex);
+                	
+                	writer.printf("Instantzia %d: Iragarpena = %s \n", i + 1, predictedClass); 
+                }
+                writer.println("Iragarpen osatuak.");
+            } 
+            System.out.println("Iragarpenak gorde dira: " + iragarpenakPath);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }    
+}
