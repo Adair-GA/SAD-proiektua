20813ed39b41de735415f341076cbaef68105571
Lauracpg <laurameihua@gmail.com>
2025-03-30 12:05:52 +0200

    Datuak eta iragarpenak

--- src/baseline_iragarpenak_egin.java
+++ src/baseline_iragarpenak_egin.java
@@ -1,7 +1,7 @@
 import java.io.FileWriter;
 import java.io.PrintWriter;
 
-import weka.classifiers.functions.LinearRegression;
+import weka.classifiers.functions.Logistic;
 import weka.core.Instances;
 import weka.core.SerializationHelper;
 import weka.core.converters.ConverterUtils.DataSource;
@@ -9,7 +9,7 @@ import weka.core.converters.ConverterUtils.DataSource;
 public class baseline_iragarpenak_egin {
     public static void main(String[] args){
         if(args.length < 3){
-            System.out.println("java -jar baseline_iragarpenak_egin.jar <testBlind.arff><model><baseline_iragarpenak.txt>");
+            System.out.println("java -jar baseline_iragarpenak_egin.jar <test_blind.arff><.model><baseline_iragarpenak.txt>");
             return;
         }
         String testPath = args[0];
@@ -18,15 +18,16 @@ public class baseline_iragarpenak_egin {
 
         try{ 
             // Linear regression eredua kargatu:
-        	LinearRegression model = (LinearRegression) SerializationHelper.read(modelPath);
+        	Logistic model = (Logistic) SerializationHelper.read(modelPath);
         	
         	//test blind kargatu:
         	DataSource src = new DataSource(testPath);
         	Instances blindData = src.getDataSet();
         	
-        	if (blindData.classIndex() == -1) {
+        	/*if (blindData.classIndex() == -1) {
         		blindData.setClassIndex(blindData.numAttributes() - 1);
-            }
+            }*/
+            blindData.setClassIndex(0);
 
             try (PrintWriter writer = new PrintWriter(new FileWriter(iragarpenakPath))) {
             	writer.println("==== Linear Regression Iragarpenak ====");
