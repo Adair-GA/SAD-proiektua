e317e518669ffdaee18495ee01b0ae6f7e299cd3
Unairc23 <unairc23@gmail.com>
2025-04-01 17:35:54 +0200

    Attribute Selection

--- src/arffEgokitu.java
+++ src/arffEgokitu.java
@@ -1,23 +1,33 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.FixedDictionaryStringToWordVector;
+import weka.filters.unsupervised.attribute.Remove;
 
 public class arffEgokitu{
     public static void main(String[] args) {
         
-        if (args.length < 2){
-            System.out.println("Erabilpena: java -jar arffEgokitu.jar <Data.arff> <hiztegia.arff>");
+        if (args.length < 3){
+            System.out.println("Erabilpena: java -jar arffEgokitu.jar <Data.arff> <Headers.arff> <hiztegia.arff>");
             System.exit(1);
         }
 
         String arffPath = args[0];
-        String hiztegiaPath = args[1];
+        String HeadersPath = args[1];
+        String hiztegiaPath = args[2];
 
         Instances data = datuakKargatu(arffPath);
+        Instances Headers = datuakKargatu(HeadersPath);
+
+        if (Headers == null) {
+            System.out.println("Headers datuak ezin izan dira kargatu.");
+            System.exit(1);
+        }
 
         if (data == null) {
             System.out.println("Datuak ezin izan dira kargatu.");
@@ -27,16 +37,24 @@ public class arffEgokitu{
 
         FixedDictionaryStringToWordVector fstwv = new FixedDictionaryStringToWordVector();
         fstwv.setDictionaryFile(new File(hiztegiaPath));
+        fstwv.setLowerCaseTokens(true);
 
         try {
             fstwv.setInputFormat(data);
             Instances dataBoW = Filter.useFilter(data, fstwv);
 
-            if (data == null) {
+            if (dataBoW == null) {
                 System.out.println("Errorea BoW sortzean.");
                 System.exit(1);
             }
             System.out.println("BoW sortu da.");
+
+            dataBoW = atributuakEgokitu(dataBoW, Headers);
+            if (dataBoW == null) {
+                System.out.println("Errorea atributuak egokitzean.");
+                System.exit(1);
+            }
+            System.out.println("Atributuak egokitu dira. \n");
 
             saveInstances(dataBoW, arffPath);
             System.out.println("BoW gordeta. \n");
@@ -51,9 +69,7 @@ public class arffEgokitu{
         try{
             DataSource source = new DataSource(path);
             Instances data = source.getDataSet();
-            if (data.classIndex() == -1) {
-                data.setClassIndex(0);
-            }
+            data.setClassIndex(0);
             return data;
         }catch (Exception e){
             e.printStackTrace();
@@ -62,11 +78,40 @@ public class arffEgokitu{
     }  
 
     public static void saveInstances(Instances data, String filePath) throws Exception {
-        String path = filePath.replace(".arff", "_BoW.arff");
+        String path = filePath.replace(".arff", "_as_BoW.arff");
         ArffSaver saver = new ArffSaver();
         saver.setInstances(data);
         System.out.println(path);
         saver.setFile(new File(path));
         saver.writeBatch();
+    }
+
+    public static Instances atributuakEgokitu(Instances data, Instances Headers) {
+        try{
+            List <String> trainHeaders = new ArrayList<String>();
+            for (int i = 0; i < Headers.numAttributes(); i++) {
+                trainHeaders.add(Headers.attribute(i).name());
+            }
+
+            List <Integer> ezabatzekoak = new ArrayList<Integer>();
+            for (int i = 0; i < data.numAttributes(); i++) {
+                if (!trainHeaders.contains(data.attribute(i).name())) {
+                    ezabatzekoak.add(i);
+                }
+            }
+            // Ezabatzeko atributuak array batean bildu
+            int[] ezabatuArray = ezabatzekoak.stream().mapToInt(Integer::intValue).toArray(); 
+
+            if (!ezabatzekoak.isEmpty()) {
+                Remove removeFilter = new Remove();
+                removeFilter.setAttributeIndicesArray(ezabatuArray);
+                removeFilter.setInputFormat(data);
+                data = Filter.useFilter(data, removeFilter);
+            }
+            return data;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- src/arffToBoW.java
+++ src/arffToBoW.java
@@ -1,8 +1,16 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.util.Dictionary;
+
+import weka.attributeSelection.InfoGainAttributeEval;
+import weka.attributeSelection.Ranker;
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
+import weka.filters.supervised.attribute.AttributeSelection;
+import weka.filters.unsupervised.attribute.Reorder;
 import weka.filters.unsupervised.attribute.StringToWordVector;
 
 public class arffToBoW {
@@ -25,22 +33,42 @@ public class arffToBoW {
         System.out.println("Datuak kargatu dira.");
 
         StringToWordVector stwv = new StringToWordVector();
-        stwv.setDictionaryFileToSaveTo(new File(hiztegiaPath));
+        // stwv.setDictionaryFileToSaveTo(new File(hiztegiaPath));
         stwv.setLowerCaseTokens(true);
-        
 
         try {
             stwv.setInputFormat(train);
             Instances trainBoW = Filter.useFilter(train, stwv);
-            System.out.println("Train BoW sortu da.");
-
-            if (train == null) {
+            if (trainBoW == null) {
                 System.out.println("Errorea BoW sortzean.");
                 System.exit(1);
             }
+            System.out.println("Train BoW sortu da. \n");
 
-            saveInstances(trainBoW, trainPath);
+            Instances trainBerria = atributuHautapena(trainBoW);
+            if (trainBerria == null) {
+                System.out.println("Errorea atributu hautapenean.");
+                System.exit(1);
+            }
+            System.out.println("Atributu hautapena egin da. \n");
+
+            Instances trainHeaders = new Instances(trainBerria);
+            trainHeaders.delete(); 
+            saveInstances(trainHeaders, trainPath.replace(".arff", "_Headers.arff"));
+            System.out.println("Train Headers gordeta. \n");
+
+            saveInstances(trainBerria, trainPath);
             System.out.println("Train BoW gordeta. \n");
+
+            // Temporal, funciona pero no es del todo correcto
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(hiztegiaPath))) {
+                // Recorrer todos los atributos y guardarlos como diccionario
+                for (int i = 0; i < trainBerria.numAttributes(); i++) { // Excluye la clase si es el primer atributo
+                    writer.write(trainBerria.attribute(i).name());
+                    writer.newLine();
+                }
+                System.out.println("Diccionario guardado en: " + hiztegiaPath);
+            }
             
         } catch (Exception e) {
             e.printStackTrace();
@@ -69,5 +97,56 @@ public class arffToBoW {
         System.out.println(path);
         saver.setFile(new File(path));
         saver.writeBatch();
+    }
+
+    public static Instances atributuHautapena(Instances data){
+        AttributeSelection as = new AttributeSelection();
+        InfoGainAttributeEval eval = new InfoGainAttributeEval();
+        Ranker ranker = new Ranker();
+        ranker.setNumToSelect(3000); 
+        ranker.setThreshold(-1.7976931348623157E308); 
+        // El valor minimo para que este threshold afecete a los atributos es mayor que 0.0, en ese punto se seleccionan 744 atributos
+        // Sin embargo, esto no parece que devuelva valores optimos en la ebaluacion (por lo menos con baseline), 
+        // los atributos optimos parecen estar sobre los 3000
+        as.setEvaluator(eval);
+        as.setSearch(ranker);
+        try{
+            as.setInputFormat(data);
+            Instances newData = Filter.useFilter(data, as);
+            System.out.println("newData: " + newData.numAttributes() + " atributu ditu atributu hautapena egin eta gero.");
+
+            // Klase berriaren posizioa gorde
+            int originalClassIndex = data.classIndex();
+            int newClassIndex = newData.classIndex();
+
+            // Klasearen posizioa aldatzekotan, birmoldatu
+            if (newClassIndex != originalClassIndex) {
+                System.out.println("Klasea posizioz aldatu da, birmoldatzen...");
+
+                // Atributuen ordena eraiki klasea lehen posizioan jarriz
+                StringBuilder order = new StringBuilder();
+                order.append(newClassIndex + 1).append(",");
+                for (int i = 0; i < newData.numAttributes(); i++) {
+                    if (i != newClassIndex) {
+                        order.append(i + 1).append(",");
+                    }
+                }
+                
+                // Reorder erabili atributu hautapena egitean azken posizioan jarri den klasea 0 posizioan jartzeko
+                Reorder reorder = new Reorder();
+                reorder.setAttributeIndices(order.toString());
+                reorder.setInputFormat(newData);
+                Instances finalData = Filter.useFilter(newData, reorder);
+                finalData.setClassIndex(finalData.numAttributes() - 1);
+
+                System.out.println("Birmoldaketa egin da, kalsea 0 posizioan dago.");
+                return finalData;
+            }
+
+            return newData;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
