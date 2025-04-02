c1b38973822eab9aec44c4cb39d936e4cb218c87
Adair-GA <adairyves@gmail.com>
2025-04-02 16:32:34 +0200

    eredu finalaren parametro ekorketa

--- eredu-finala/src/GetModel.java
+++ eredu-finala/src/GetModel.java
@@ -1,43 +1,33 @@
-import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.functions.SMO;
 import weka.classifiers.functions.supportVector.*;
 import weka.core.Instances;
-import weka.core.Utils;
+import weka.core.SerializationHelper;
 import weka.core.converters.ConverterUtils;
 
-import java.util.Arrays;
-import java.util.Random;
 
-public class FSS {
+public class GetModel {
 	public static void main(String[] args) throws Exception {
-		ConverterUtils.DataSource ds = new ConverterUtils.DataSource("datuak/train_BoW.arff");
-		ConverterUtils.DataSource testDs = new ConverterUtils.DataSource("datuak/dev_BoW.arff");
+		if(args.length != 3){
+			System.out.println("Usage: java -jar GetModel.jar <input>train.arff <input>dev.arff <output>model");
+		}
+		
+		ConverterUtils.DataSource ds = new ConverterUtils.DataSource(args[0]);
+		ConverterUtils.DataSource testDs = new ConverterUtils.DataSource(args[1]);
 
 		Instances test = testDs.getDataSet();
 		Instances data = ds.getDataSet();
 		data.setClassIndex(0);
 		test.setClassIndex(0);
 
-		int minClassIndex = Utils.minIndex(data.attributeStats(data.classIndex()).nominalCounts);
 		Kernel[] kernels = new Kernel[] {new PolyKernel(), new NormalizedPolyKernel()};
 
-//		for (int i = -50; i < 50; i = i + 10) {
-//			double tolerance = 1.0e-3;
-//			tolerance += i * 2e-5;
-//			if (tolerance <= 0 ){
-//				continue;
-//			}
-//			double result = evaluateModel(data, test, tolerance, new NormalizedPolyKernel(), minClassIndex);
-//			System.out.printf("Tolerance: %f, result: %f\n", tolerance, result);
-//		}
-
-		Kernel bestKernel = null;
+		Kernel bestKernel = kernels[0];
 		double bestExponent = Double.MIN_VALUE;
 		double bestScore = Double.MIN_VALUE;
 
 		for (Kernel kernel : kernels) {
-			for (int i = -50; i < 50; i++) {
+			for (int i = 0; i < 100; i= i + 2) {
 				double exponent = 1.0 + ((double) i / 100);
 				double result = evaluateModel(data, test, kernel, exponent);
 
@@ -51,6 +41,16 @@ public class FSS {
 		}
 
 		System.out.printf("Best kernel: %s, best exponent: %f, achieved weighted f-measure: %f", bestKernel.getClass().getSimpleName(), bestExponent, bestScore);
+
+		SMO classifier = new SMO();
+		bestKernel.setOptions(new String[]{"-E", Double.toString(bestExponent)});
+
+		classifier.setKernel(bestKernel);
+
+		classifier.buildClassifier(data);
+
+
+		SerializationHelper.write(args[2], classifier);
 	}
 
 
